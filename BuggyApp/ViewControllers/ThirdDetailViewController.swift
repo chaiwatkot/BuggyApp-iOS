30a0143b6650c25cc200fd32ef1cfa9b17d05504
chaiwat chanthasen <tagotstroke100m@gmail.com>
2019-08-19 17:01:53 +0700

    no message

---
+++ BuggyApp/ViewControllers/ThirdDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdDetailViewController.swift
+//  BuggyApp
+//
+//  Created by Chaiwat Chanthasen on 19/8/2562 BE.
+//  Copyright © 2562 scbeasy. All rights reserved.
+//
+
+import UIKit
+
+class ThirdDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
